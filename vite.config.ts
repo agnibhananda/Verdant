aafc77cd88bc4066a29c8690be7b349070910da2
Agnibha Nanda <64578820+agnibhananda@users.noreply.github.com>
2025-01-18 23:26:03 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,18 @@ export default defineConfig({
   plugins: [react()],
   server: {
     open: true
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'chart-vendor': ['recharts'],
+          'animation-vendor': ['framer-motion']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000,
+    sourcemap: true
   }
 });
