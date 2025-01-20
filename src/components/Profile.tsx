48e7dbfa6e5f7376a2e0cb681510aff6ee004e21
Agnibha Nanda <64578820+agnibhananda@users.noreply.github.com>
2025-01-20 13:02:09 +0530

    Update Profile.tsx

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -14,7 +14,7 @@ const Profile = () => {
     joinDate: 'January 2025',
     completedChallenges: 12,
     currentStreak: 15,
-    bio: 'Passionate about environmental conservation and sustainable living.',
+    bio: 'i like to build cool stuff',
     email: 'agnibhananda@gmail.com'
   });
 
